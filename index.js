5574555cb09955f90f3149710815c777f62b3511
Paul Serby <paul.serby@clock.co.uk>
2011-06-23 21:01:35 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * Paul Serby <paul.serby@clock.co.uk>
+ *
+ * New BSD Licensed
+ *
+ * Thursday 23 June 2011
+ */
+
+exports.collectBody = require('./lib/body-collector');
---
+++ test/body-collector.test.js
@@ -0,0 +1,31 @@
+var
+	httpUtils = require('../index'),
+	assert = require('assert');
+
+module.exports = {
+	'success': function() {
+
+		var http = require('http');
+		var server = http.createServer(function (req, res) {
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end('Hello World\n');
+		});
+		server.listen(1337, 'localhost');
+
+		var options = {
+			host: 'localhost',
+			port: 1337,
+			path: '/',
+			method: 'GET'
+		};
+
+		var req = http.request(options, function(res) {
+			res.setEncoding('utf8');
+			httpUtils.collectBody(res, function(data) {
+				assert.equal('Hello World\n', data);
+				server.close();
+			});
+		});
+		req.end();
+	}
+};
